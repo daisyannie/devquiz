0d276a1404cb82f175a823df5d8e0400c88e34af
Daisy <daisy@vizir.com.br>
2021-04-21 11:42:54 -0300

    feat: add json database

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,7 +1,5 @@
-import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/home_repository.dart';
 import 'package:devquiz/home/home_state.dart';
-import 'package:devquiz/shared/models/answer_model.dart';
-import 'package:devquiz/shared/models/question_model.dart';
 import 'package:devquiz/shared/models/quiz_model.dart';
 import 'package:devquiz/shared/models/user_model.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,56 +12,17 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    user = UserModel(
-      name: "Daisy", 
-      photoUrl: "https://avatars.githubusercontent.com/u/26805544?v=4"
-    );
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [
-      QuizModel(
-        image: AppImages.blocks,
-        title: "NLW 5 Flutter",
-        questionAnswered: 1,
-        level: Level.facil,
-        questions: [
-          QuestionModel(
-            title: "Que cor é o céu?",
-            answers: [
-              AnswerModel(title: "Verde"),
-              AnswerModel(title: "Azul", isRight: true),
-              AnswerModel(title: "Verde"),
-              AnswerModel(title: "Vermelho"),
-            ]
-          ),
-          QuestionModel(
-            title: "Que cor é o céu?",
-            answers: [
-              AnswerModel(title: "Verde"),
-              AnswerModel(title: "Azul", isRight: true),
-              AnswerModel(title: "Verde"),
-              AnswerModel(title: "Vermelho"),
-            ]
-          ),
-          QuestionModel(
-            title: "Que cor é o céu?",
-            answers: [
-              AnswerModel(title: "Verde"),
-              AnswerModel(title: "Azul", isRight: true),
-              AnswerModel(title: "Verde"),
-              AnswerModel(title: "Vermelho"),
-            ]
-          ),
-        ],
-      ),
-    ];
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
