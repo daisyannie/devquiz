10dc80eaee3acd966ac871262ff626c977f83ad4
Daisy <daisy@vizir.com.br>
2021-04-22 21:14:43 -0300

    feat: add navigation page

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,34 +1,44 @@
 import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:devquiz/core/core.dart';
+import 'package:devquiz/shared/models/answer_model.dart';
+import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+  const QuizWidget({Key? key, required this.question, required this.onChange}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
     return Container(
      child: Column(
        children: [
-         Text(title, style: AppTextStyles.heading),
+         SizedBox(height: 64,),
+         Text(widget.question.title, style: AppTextStyles.heading),
          SizedBox(height: 24,),
-         AnswerWidget(
-           title: "Possibilita a criação de aplicativos compilados nativamente",
-           isSelected: true,
-           isRight: false,
-         ),
-         AnswerWidget(
-           title: "Possibilita a criação de aplicativos compilados nativamente",
-         ),
-         AnswerWidget(
-           title: "Possibilita a criação de aplicativos compilados nativamente",
-         ),
-         AnswerWidget(
-           title: "Possibilita a criação de aplicativos compilados nativamente",
-         )
+         for(var index = 0; index < widget.question.answers.length; index++)
+          AnswerWidget(
+            answer: answer(index),
+            disabled: indexSelected != -1,
+            isSelected: indexSelected == index,
+            onTap: () {
+              indexSelected = index;
+              setState(() {});
+              Future.delayed(Duration(seconds: 1)).then((value) => widget.onChange());
+            },
+          ),
        ],
-     )
+     ),
     );
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge/challenge_page.dart';
 import 'package:devquiz/core/core.dart';
 import 'package:devquiz/home/home_controller.dart';
 import 'package:devquiz/home/home_state.dart';
@@ -64,6 +65,9 @@ class _HomePageState extends State<HomePage> {
                       title: quiz.title, 
                       percent: quiz.questionAnswered/quiz.questions.length,
                       completed: "${quiz.questionAnswered}/${quiz.questions.length}",
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => ChallengePage(questions: quiz.questions)));
+                      },
                     )).toList(),
                 )
               ),
